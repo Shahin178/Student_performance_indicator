4a9e0d36d00f562311d8d7bd875e8ac8a0e71898
Shahin Bano <banoshahin89@gmail.com>
2026-08-23 01:10:46 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -80,3 +80,11 @@ def evaluate_model(
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
